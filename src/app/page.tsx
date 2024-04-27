154ae351cc15144c3a0e2b96d432aa7f202cd7d9
Vipul Goel <vipul2008@gmail.com>
2024-04-27 11:54:18 +0530

    UI created for Equity Calculator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,125 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-4">
+			<div className="flex flex-col items-center p-8 min-w-md">
+				{/* form heading */}
+				<h1 className="font-bold text-2xl mb-4 text-center">
+					Equity Calculator
+				</h1>
+				{/* First row - radio buttons */}
+				<div className="flex p-2 mb-4 items-center gap-4 flex-wrap">
+					<label className="flex items-center gap-1">
+						<input type="radio" name="type" value="price" />
+						<span>Buy Price</span>
+					</label>
+					<label className="flex items-center gap-1">
+						<input type="radio" name="type" value="unit" />
+						<span>Units</span>
+					</label>
+					<label className="flex items-center gap-1">
+						<input type="radio" name="type" value="investment" />
+						<span>Investment</span>
+					</label>
+				</div>
+				{/* Second row - inputs */}
+				<div className="flex justify-center gap-2 flex-wrap w-full">
+					<div className="flex flex-col p-6 space-y-2 bg-white shadow-md rounded-lg flex-grow border border-green-500">
+						<label
+							htmlFor="price1"
+							className="text-sm font-semibold text-gray-600"
+						>
+							Buy Price
+						</label>
+						<input
+							type="number"
+							id="price1"
+							className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+						/>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+						<label
+							htmlFor="units1"
+							className="text-sm font-semibold text-gray-600"
+						>
+							Units
+						</label>
+						<input
+							type="number"
+							id="units1"
+							className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+						/>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+						<label
+							htmlFor="investment1"
+							className="text-sm font-semibold text-gray-600"
+						>
+							Investment
+						</label>
+						<input
+							type="number"
+							id="investment1"
+							className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+						/>
+					</div>
+					<div className="flex flex-col p-6 space-y-2 bg-white shadow-md rounded-lg flex-grow border border-yellow-400">
+						<label
+							htmlFor="price2"
+							className="text-sm font-semibold text-gray-600"
+						>
+							Buy Price
+						</label>
+						<input
+							type="number"
+							id="price2"
+							className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
+						/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+						<label
+							htmlFor="units2"
+							className="text-sm font-semibold text-gray-600"
+						>
+							Units
+						</label>
+						<input
+							type="number"
+							id="units2"
+							className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
+						/>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+						<label
+							htmlFor="investment2"
+							className="text-sm font-semibold text-gray-600"
+						>
+							Investment
+						</label>
+						<input
+							type="number"
+							id="investment2"
+							className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500"
+						/>
+					</div>
+				</div>
+				{/* Third row - output labels */}
+				<div className="flex flex-col items-start justify-items-start w-full my-4">
+					<span>Average Buy Price :</span>
+					<span>Total Units :</span>
+					<span>Total Investment :</span>
+				</div>
+				{/* Fourth Row - Action buttons */}
+				<div className="flex items-center gap-4 m-2 w-full justify-evenly">
+					<button
+						type="submit"
+						className="flex-grow basis-1/2 p-2 bg-green-700 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-50"
+					>
+						Calculate
+					</button>
+					<button
+						type="submit"
+						className="flex-grow basis-1/2 p-2 bg-blue-700 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50"
+					>
+						Clear
+					</button>
+				</div>
+			</div>
+		</main>
+	);
 }
